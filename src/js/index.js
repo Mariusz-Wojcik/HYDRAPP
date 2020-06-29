df29f2a00f50a13177385d98772e0707e5642336
Mariusz Wójcik <36dragan@gmail.com>
2020-06-29 22:44:46 +0200

    scss modification & jv

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,33 @@ registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const glassCounter = document.querySelector('.glass__counter--js');
+const buttonPlus = document.querySelector('.container__button-plus--js');
+const buttonMinus = document.querySelector('.container__button-minus--js');
+const key = new Date().toISOString().slice(0, 10);
+
+let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue){
+    currentGlassCounter=localStorageValue;}
+    else {
+        localStorage.setItem(key, 0);
+    }
+
+    glassCounter.innerHTML = localStorageValue;
+
+buttonPlus.addEventListener('click',()=>{
+    currentGlassCounter++;
+    glassCounter.innerHTML=currentGlassCounter; 
+    localStorage.setItem(key, currentGlassCounter);
+})
+
+buttonMinus.addEventListener('click',()=>{
+    if (currentGlassCounter > 0 ){
+    currentGlassCounter--;}
+    glassCounter.innerHTML=currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
+}
+)
